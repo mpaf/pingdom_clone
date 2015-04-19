ce5bd57e51dc8622489fc6a76cf582f87d59c88f
Miguel <mpaf98@gmail.com>
2015-04-20 00:33:05 +0300

    added comments

--- models.py
+++ models.py
@@ -13,30 +13,32 @@ SAVED_SITES_FILE = 'saved_sites.pkl'
 # Frequency at which Site data is saved to disk
 DUMP_RATE = 30
 
-# This variable keeps the list of Site objects
-# and their all-time response times in memory
-# during the application run.
-
 class Site(object):
+  """ Defines the model for a Site
 
+      Stores the last status, as well as a list of reponse times in seconds
+      (float precision)"""
+
+  # For printing a Site object in a pleasant way
   def __repr__(self):
     return "<site url:'{0}' content_str '{1}'>".format(self.url, self.content_str)
+  # defines equality for objects of this class
   def __eq__(self, other):
     return self.url == other.url
-  # hash of object to allow creation of sets
+  # hash of object to allow creation of sets (equality in lists)
   def __hash__(self):
      return hash(self.url)
 
   def __init__(self, url, content_str):
-    self.url = url
-    self.content_str = content_str
-    self.resp_time=[]
-    self.last_http_code = None
-    self.string_matched = False
-    self.encoding = None
+    self.url = url  # the site url
+    self.content_str = content_str  # the string to match
+    self.resp_time=[]  # the response time
+    self.last_http_code = None  # the http code received
+    self.string_matched = False  # did the string match?
+    self.encoding = None  # what was the site encoding
 
   def dump_site_for_inspection(self):
-    ''' dump the html for a site '''
+    """ dump the html for a site to disk """
     import requests
     r = requests.get(self.url)
     # remove http and .com, .net etc. from file name
@@ -44,11 +46,14 @@ class Site(object):
       f.write(r.text)
 
   def add_time(self, response_time):
+    """ adds response_time to the array of times """
     if type(response_time) != float:
       raise TypeError("response time must be a float")
     self.resp_time.append([time.time(), response_time*1000.0])
 
 def pickle_to_file(sites):
+  """ this function stores a collection of Site to disk
+      as pickled Python objs """
   if sites:
     if type(sites) == list:
       if type(sites[0]) == Site:
@@ -58,22 +63,19 @@ def pickle_to_file(sites):
           return
   raise TypeError("Site list empty or of incorrect type")
 
-# No worries passing variables referencing large
-# data to functions, as Python passes it by reference
 def dump_sites(sites, dump_rate=DUMP_RATE):
-    ''' pickle Site objects to the filesystem
-        ran periodically at dump_rate, or when
-        app exits '''
+  """ periodically at dump_rate, save Site
+      objects to the filesystem """
 
-    logger.debug("saving all site data to disk")
-    pickle_to_file(sites)
-    t = threading.Timer(dump_rate, dump_sites, [sites])
-    t.daemon=True
-    t.start()
-    return
+  logger.debug("saving all site data to disk")
+  pickle_to_file(sites)
+  t = threading.Timer(dump_rate, dump_sites, [sites])
+  t.daemon=True
+  t.start()
+  return
 
 def get_pickled_sites():
-
+  """ retrieve Site objs from pickled file data """
   # Try to get list of Site models
   try:
     with open(SAVED_SITES_FILE, 'rb') as input:
@@ -87,6 +89,8 @@ def get_pickled_sites():
   return saved_sites
 
 def configed_sites(sites_dict):
+  """ populate list of Site objs, based on configuration file,
+      Duplicate urls will be discarded """
   cfg_sites = []
 
   for site in sites_dict:
@@ -101,8 +105,9 @@ def configed_sites(sites_dict):
   return cfg_sites
 
 def get_sites(cfg_sites_dict):
-  ''' change cfg_sites to add possible data series saved
-      to disk '''
+  """ Match configured sites to data stored on disk. Populate
+      configured sites with possible historical data, when urls
+      match """
 
   cfg_sites = configed_sites(cfg_sites_dict)
   saved_sites = get_pickled_sites()
